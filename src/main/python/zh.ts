05c060c6801264d843a2b26813d8a167fcb1e3bc
zhaqian <80608733+zhaqian12@users.noreply.github.com>
2022-03-16 22:26:36 +0800

    Create zh.ts

---
+++ src/main/python/zh.ts
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="any_keycode_dialog.py" line="13"/>
+        <source>AnyKeycodeDialog</source>
+        <comment>Enter an arbitrary keycode</comment>
+        <translatorcomment>输入任意键值</translatorcomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="any_keycode_dialog.py" line="47"/>
+        <source>AnyKeycodeDialog</source>
+        <comment>Enter an expression</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="any_keycode_dialog.py" line="50"/>
+        <source>AnyKeycodeDialog</source>
+        <comment>Invalid input: {}</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="any_keycode_dialog.py" line="53"/>
+        <source>AnyKeycodeDialog</source>
+        <comment>Computed value: 0x{:X}</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="any_keycode_dialog.py" line="56"/>
+        <source>AnyKeycodeDialog</source>
+        <comment>Invalid input</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="55"/>
+        <source>MainWindow</source>
+        <comment>Refresh</comment>
+        <translatorcomment>刷新</translatorcomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="294"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="132"/>
+        <source>MenuFile</source>
+        <comment>Load saved layout...</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="136"/>
+        <source>MenuFile</source>
+        <comment>Save current layout...</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="140"/>
+        <source>MenuFile</source>
+        <comment>Sideload VIA JSON...</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="143"/>
+        <source>MenuFile</source>
+        <comment>Download VIA definitions</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="146"/>
+        <source>MenuFile</source>
+        <comment>Load dummy JSON...</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="149"/>
+        <source>MenuFile</source>
+        <comment>Exit</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="153"/>
+        <source>Menu</source>
+        <comment>File</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="163"/>
+        <source>MenuSecurity</source>
+        <comment>Unlock</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="166"/>
+        <source>MenuSecurity</source>
+        <comment>Lock</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="169"/>
+        <source>MenuSecurity</source>
+        <comment>Reboot to bootloader</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="172"/>
+        <source>Menu</source>
+        <comment>Keyboard layout</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="176"/>
+        <source>KeyboardLayout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="188"/>
+        <source>Menu</source>
+        <comment>Security</comment>
+        <translatorcomment>安全</translatorcomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="194"/>
+        <source>Menu</source>
+        <comment>Theme</comment>
+        <translatorcomment>主题</translatorcomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="198"/>
+        <source>MenuTheme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="208"/>
+        <source>MenuAbout</source>
+        <comment>About Vial...</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="210"/>
+        <source>Menu</source>
+        <comment>About</comment>
+        <translatorcomment>关于</translatorcomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="360"/>
+        <source>MainWindow</source>
+        <comment>In order to fully apply the theme you should restart the application.</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
