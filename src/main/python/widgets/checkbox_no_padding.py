e19a649c038ef28aa54cd4b4651a5dd9c0df3f25
zhaqian <80608733+zhaqian12@users.noreply.github.com>
2022-05-18 23:27:41 +0800

    delete

--- src/main/python/widgets/checkbox_no_padding.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-from PyQt5.QtWidgets import QCheckBox
-
-
-class CheckBoxNoPadding(QCheckBox):
-
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.setStyleSheet("QCheckBox { padding: 0; }")
